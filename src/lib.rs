b8a99ebc71380b5f15a600344f0c373e785eeecf
Raui Ghazaleh <r.ghazaleh@physens.de>
2023-09-03 10:10:11 +0200

    Added "accuracy" feature for more accurate snoozing

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,27 @@
+//! Accurate intermittent sleeping and returning the control flow inbetween.
+//! Sleeping for the duration of `len` or up to `self.total`.
+//! Won't sleep longer than `len` within accuracy of the operation system.
+//!
+//! For higher accuracy try the feature `accuracy` and `accurate_snooze` which
+//! utilizes [spin_sleep](https://crates.io/crates/spin_sleep).
+//!
+//! # Examples
+//! ```
+//! use std::time::{Duration, Instant};
+//! let total = Duration::from_secs(1);  // Sleeping for a total of 1 s
+//! let len = Duration::from_millis(300);  // Interrupting the sleep after 100 ms
+//! let int = isleep::IntermittentSleeping::new(total);
+//! let start = Instant::now();
+//! let mut counter = 0;
+//! while int.snooze(len) {
+//!     counter += 1;  // Doing something while "sleeping"
+//! }
+//! // Sleeps approximately 300, 300, 300 and 100 ms ≈ `total`
+//! assert!(start.elapsed().lt(&Duration::from_millis(1200)));
+//! assert_eq!(counter, 4);
+//! ```
 use std::time::{Duration, Instant};
+
 
 /// Intermittent sleeping for a specified duration since struct instantiation
 pub struct IntermittentSleeping {
@@ -9,25 +32,7 @@ pub struct IntermittentSleeping {
 }
 
 impl IntermittentSleeping {
-    /// Intermittent sleeping and returning the control flow inbetween.
-    /// Sleeping for the duration of `len` or up to `self.total`.
-    /// Won't sleep longer than `len` within the accuracy of the operating system.
-    ///
-    /// # Examples
-    /// ```
-    /// use std::time::{Duration, Instant};
-    /// let total = Duration::from_secs(1);  // Sleeping for a total of 1 s
-    /// let len = Duration::from_millis(300);  // Interrupting the sleep after 100 ms
-    /// let int = isleep::IntermittentSleeping::new(total);
-    /// let start = Instant::now();
-    /// let mut counter = 0;
-    /// while int.snooze(len) {
-    ///     counter += 1;  // Doing something while "sleeping"
-    /// }
-    /// // Sleeps approximately 300, 300, 300 and 100 ms ≈ `total`
-    /// assert!(start.elapsed().lt(&Duration::from_millis(1200)));
-    /// assert_eq!(counter, 4);
-    /// ```
+    /// Sleeps for `len` or up to `self.total` within accuracy of the OS.
     pub fn snooze(&self, len: Duration) -> bool {
         match self.total.checked_sub(self.start.elapsed()) {
             Some(dt) => {
@@ -44,4 +49,33 @@ impl IntermittentSleeping {
             start: Instant::now(),
         }
     }
+
+    #[cfg(feature = "accuracy")]
+    /// Sleeps for `len` or up to `self.total` with high accuracy utilizing spin locks.
+    pub fn accurate_snooze(&self, len: Duration) -> bool {
+        match self.total.checked_sub(self.start.elapsed()) {
+            Some(dt) => {
+                spin_sleep::sleep(len.min(dt));
+                true
+            }
+            None => false,
+        }
+    }
+}
+
+#[cfg(feature = "accuracy")]
+#[test]
+fn test_accuracy() {
+    let responsiveness = Duration::from_micros(1);
+    let mut counter_1 = 0;
+    let mut counter_2 = 0;
+    let int = IntermittentSleeping::new(Duration::from_secs(1));
+    while int.snooze(responsiveness) {
+        counter_1 += 1;
+    }
+    let int = IntermittentSleeping::new(Duration::from_secs(1));
+    while int.accurate_snooze(responsiveness) {
+        counter_2 += 1;
+    }
+    assert!(counter_1 <= counter_2);
 }
